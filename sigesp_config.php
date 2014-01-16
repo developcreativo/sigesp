66fdea23dba793cef8b02635031c47460d2583eb
root <root@localhost.localdomain>
2014-01-16 17:50:28 -0430

    Primera Subida

---
+++ sigesp_config.php
@@ -0,0 +1,16 @@
+<?php
+$i=1;
+
+$empresa["hostname"][$i]="192.168.0.21";
+$empresa["port"][$i]="5432";
+$empresa["database"][$i]="SIGESPTIL";
+$empresa["login"][$i]="postgres";
+//KEYNOTE:: BACK TO PAST
+//$empresa["password"][$i]="123456";
+$empresa["password"][$i]="Or1noqu1a";
+$empresa["gestor"][$i]="postgres";
+$empresa["width"][$i]="70";
+$empresa["height"][$i]="70";
+$empresa["logo"][$i]="logo.jpg";
+
+?>
